62d3cd07c38d2f7ec94b189026f1439f1c7f1c96
Iyanuoluwa Odelotan <iyanuodelotan@gmail.com>
2022-06-21 06:29:48 +0100

    create calculator

---
+++ script.js
@@ -0,0 +1,49 @@
+function addition(r,t){
+    return r + t;
+}
+function subtraction(r,t){
+    return r - t;
+}
+
+function multiplication(r,t){
+    return r * t;
+}
+
+function division(r,t){
+    return r / t;
+}
+function remainder(r,t){
+    return r % t;
+}
+
+function calculator(){
+    var r = Number(prompt("WELCOME TO YOUR CALCULATOR \n Enter the first number: "));
+    var t = Number(prompt("Enter the second number: "));
+    var sign = prompt("Enter arithmetic sign: ");
+    var result = 0;
+    if (sign == "+"){
+        result = addition(r,t);
+    }
+    else if (sign == "-"){
+        result = subtraction(r,t);
+    }
+    else if (sign == "*"){
+        result = multiplication(r,t);
+    }
+    else if (sign == "/"){
+        result = division(r,t);
+    }
+    else if (sign == "%"){
+        result = remainder(r,t);
+    }
+    else{
+        alert("Please enter a valid number or arithmetic sign")
+    }
+    alert(result);
+    document.getElementById("first").innerHTML = r;
+    document.getElementById("operator").innerHTML = sign;
+    document.getElementById("second").innerHTML = t;
+    document.getElementById("answer").innerHTML = result;
+}
+
+calculator();
