7cd3363f045bebc46a4b8f8241bfbef45f9f7b2d
Moidz <iyanuodelotan@gmail.com>
2022-06-29 15:02:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-function addition(r,t){
-    return r + t;
+function add(m,d){
+    return m + d;
 }
-function subtraction(r,t){
-    return r - t;
-}
-
-function multiplication(r,t){
-    return r * t;
+function subtract(m,d){
+    return m - d;
 }
 
-function division(r,t){
-    return r / t;
-}
-function remainder(r,t){
-    return r % t;
+function multiply(m,d){
+    return m * d;
 }
 
-function calculator(){
-    let r = Number(prompt("WELCOME TO YOUR CALCULATOR \n Enter the first number: "));
-    let t = Number(prompt("Enter the second number: "));
+function divide(m,d){
+    return m / d;
+}
+function remain(m,d){
+    return m % d;
+}
+
+function calculate(){
+    let m = Number(prompt("WELCOME TO YOUR CALCULATOR \n Enter the first number: "));
+    let d = Number(prompt("Enter the second number: "));
     let sign = prompt("Enter arithmetic sign: ");
     let result = 0;
     if (sign == "+"){
-        result = addition(r,t);
+        result = add(m,d);
     }
     else if (sign == "-"){
-        result = subtraction(r,t);
+        result = subtract(m,d);
     }
     else if (sign == "*"){
-        result = multiplication(r,t);
+        result = multiply(m,d);
     }
     else if (sign == "/"){
-        result = division(r,t);
+        result = divide(m,d);
     }
     else if (sign == "%"){
-        result = remainder(r,t);
+        result = remain(m,d);
     }
     else{
         alert("Please enter a valid number or arithmetic sign/operator");
     }
-    alert(r + " " + sign + " " + t);
+    alert(m + " " + sign + " " + d);
     alert("Result: " + result);
 }
 
-calculator();
+calculate();
